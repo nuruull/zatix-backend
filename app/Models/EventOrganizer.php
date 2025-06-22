c07fc878e25bf173e33c6a6ed67c7f1286438ef1
nuruull <nurul21ti@mahasiswa.pcr.ac.id>
2025-06-22 18:22:37 +0700

    Event Organizer | add function to model

--- app/Models/EventOrganizer.php
+++ app/Models/EventOrganizer.php
@@ -2,18 +2,38 @@
 
 namespace App\Models;
 
+use Spatie\Activitylog\LogOptions;
+use App\Enum\Type\OrganizerTypeEnum;
 use Spatie\Permission\Traits\HasRoles;
+use App\Enum\Status\DocumentStatusEnum;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
 
 class EventOrganizer extends Model
 {
     use HasFactory, HasRoles, LogsActivity;
 
-    protected $fillable = ['eo_owner_id', 'name', 'logo', 'description', 'email_eo', 'phone_no_eo', 'address_eo', 'is_doc_verified'];
+    protected $fillable = ['eo_owner_id', 'organizer_type', 'name', 'logo', 'description', 'email_eo', 'phone_no_eo', 'address_eo', 'is_doc_verified'];
+
+    protected $casts = [
+        'organizer_type' => OrganizerTypeEnum::class,
+    ];
+
+    protected $appends = ['is_verified'];
+
+    public function getIsVerifiedAttribute(): bool
+    {
+        $requiredDocs = ($this->organizer_type === OrganizerTypeEnum::INDIVIDUAL)
+            ? ['ktp']
+            : ['npwp', 'nib'];
+
+        $verifiedDocs = $this->documents()
+            ->where('status', DocumentStatusEnum::VERIFIED)
+            ->pluck('type');
+        return count(array_intersect($requiredDocs, $verifiedDocs->all())) === count($requiredDocs);
+    }
 
     //create log acativity for event organizer model
     public function getActivitylogOptions(): LogOptions
@@ -35,7 +55,8 @@ class EventOrganizer extends Model
         return $this->morphMany(Document::class, 'documentable');
     }
 
-    public function members() {
+    public function members()
+    {
         return $this->belongsToMany(User::class, 'event_organizer_users', 'eo_id', 'user_id');
     }
 }
