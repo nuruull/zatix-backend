e590c58199d5cc492630d86739eae0c26614c439
nuruull <nurul21ti@mahasiswa.pcr.ac.id>
2025-06-24 16:26:14 +0700

    Document | modify document factory for testing

--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -2,12 +2,12 @@
 
 namespace Database\Factories;
 
-use App\Models\EventOrganizer;
 use App\Enum\Status\DocumentStatusEnum;
+use App\Models\EventOrganizer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EventOrganizer>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Document>
  */
 class DocumentFactory extends Factory
 {
@@ -19,16 +19,65 @@ class DocumentFactory extends Factory
     public function definition(): array
     {
         return [
-            'documentable_type' => EventOrganizer::class,
-            'documentable_id' => EventOrganizer::factory(),
-            'type' => $this->faker->randomElement(['ktp', 'npwp', 'nib']),
             'file' => 'documents/' . $this->faker->uuid() . '.pdf',
-            'number' => $this->faker->numerify('################'),
-            'name' => $this->faker->name(),
-            'address' => $this->faker->address(),
-            'status' => DocumentStatusEnum::PENDING,
+            'status' => $this->faker->randomElement(DocumentStatusEnum::cases()),
             'reason_rejected' => null,
         ];
+    }
 
+    /**
+     * Configure the model factory.
+     * Logika ini akan berjalan SETELAH model dibuat.
+     */
+    public function configure(): static
+    {
+        return $this->afterMaking(function ($document) {
+            // Jika tidak ada nama/alamat, ambil dari induknya (EO)
+            if (!$document->name && $document->documentable) {
+                $document->name = $document->documentable->name;
+            }
+            if (!$document->address && $document->documentable) {
+                $document->address = $document->documentable->address_eo;
+            }
+        });
+    }
+
+    /**
+     * State untuk dokumen KTP.
+     */
+    public function forKtp(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'ktp',
+                'number' => $this->faker->nik(),
+            ];
+        });
+    }
+
+    /**
+     * State untuk dokumen NPWP.
+     */
+    public function forNpwp(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'npwp',
+                'number' => $this->faker->numerify('##.###.###.#-###.###'),
+            ];
+        });
+    }
+
+    /**
+     * State untuk dokumen NIB.
+     */
+    public function forNib(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'nib',
+                'number' => $this->faker->numerify('##############'),
+            ];
+        });
     }
 }
