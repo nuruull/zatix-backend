b120d6092d8bc72029d14788b6e708e098d571e2
nuruull <nurul21ti@mahasiswa.pcr.ac.id>
2025-06-27 18:37:36 +0700

    Iterasi 5 | US 31 | make migration for e-tickets Table

---
+++ database/migrations/2025_06_27_182728_create_e_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('e_tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_code')->unique(); // Kode unik untuk QR Code
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('ticket_id'); // Jenis tiketnya
+            $table->string('attendee_name')->nullable();
+            $table->timestamp('checked_in_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('e_tickets');
+    }
+};
