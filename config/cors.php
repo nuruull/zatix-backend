ca095c81eb4713b35b8c858de812f0c1d447b45f
nuruull <nurul21ti@mahasiswa.pcr.ac.id>
2025-07-24 12:22:39 +0700

    Perbaiki cors untuk frontend | ganti allowed origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://www.zatix.id'],
     // 'allowed_origins' => [env('ALLOWED_ORIGIN')],
 
     'allowed_origins_patterns' => [],
@@ -30,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
